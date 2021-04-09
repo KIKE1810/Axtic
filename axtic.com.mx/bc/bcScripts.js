2d8bd404b7e5ae4c8293f1c7d3efa70e39ed70e2
Cesar Teran <hijazodemividaza@gmail.com>
2021-04-08 22:12:10 -0500

    envia el correo

--- axtic.com.mx/bc/bcScripts.js
+++ axtic.com.mx/bc/bcScripts.js
@@ -1,45 +1,57 @@
-$(function () {
+$(document).ready(function (){
+    $('#myloader').hide();
+    //Close the bootstrap alert
+    $('#linkClose').click(function () {
+        $('#divError').hide('fade');
+    });
 
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-    $('#contact-form').validator();
+    var f = new Date();
 
 
-    // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
 
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "bc/contact.php";
-
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
-
-                    // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
-    })
 });
+
+//$(function setAction () {
+    // Save the new user details
+    //$('#btnContacto').click(function () {
+        $('#contact-form').on('submit', function () {
+            
+            $('#myloader').show();
+            var url = "http://tics-tula.com/contacto/api/mail";
+            $.ajax({
+                    url: url,
+                    method: 'POST',
+                    //contentType: 'application/json',
+                    data: {
+                        nombre: $('#form_name').val(),
+                        telefono: $('#form_tel').val(),
+                        email: $('#form_email').val(),
+                        mensaje: $('#form_message').val(),
+                        areaInteres: $('#form_need').val()
+                        
+                    },
+                    success: function () {
+                      
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                        $('#successModal').modal('show');
+                        $('#myloader').hide();
+                       
+                        //window.location.href = "/index.html";//aqui va al inde
+                    },
+                    error: function (jqXHR) {
+                        $('#divErrorText').text(jqXHR.responseText);
+                        $('#divError').show('fade');
+                        $('#myloader').hide();
+                        alert('Algo salio mal')
+                    }
+                });
+                $('#btnCerrar').click(function () {
+                    window.location.href = "axtic/contacto.htm"
+                });
+        
+           
+        
+
+});
+
+
